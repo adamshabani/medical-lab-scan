2586242c92de8e5c0d2151978d8a4fc7b34d5ae4
theomaro <theo.pombe@gmail.com>
2024-03-24 11:27:07 +0300

    design details modal

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,6 +1,17 @@
 "use strict";
-const sayHello = (name) => {
-    console.log("Hello " + name);
-};
-sayHello("World");
+const modalToggler = document.querySelectorAll(".modal-toggler");
+const modalContainer = document.querySelector("#default-modal");
+let modalTitle = document.querySelector("#modal-title");
+const modalClose = document.querySelector(".modal-close");
+modalClose.addEventListener("click", () => {
+    modalContainer.classList.add("hidden");
+});
+modalToggler.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        var _a, _b;
+        const target = e.currentTarget;
+        modalTitle.textContent = (_b = (_a = target.textContent) === null || _a === void 0 ? void 0 : _a.trim()) !== null && _b !== void 0 ? _b : "";
+        modalContainer.classList.remove("hidden");
+    });
+});
 //# sourceMappingURL=index.js.map
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,23 @@
-const sayHello = (name: string): void => {
-    console.log("Hello " + name);
-}
+const modalToggler = document.querySelectorAll(
+  ".modal-toggler"
+) as NodeListOf<HTMLButtonElement>;
+const modalContainer = document.querySelector(
+  "#default-modal"
+) as HTMLDivElement;
 
-sayHello("World")
+let modalTitle = document.querySelector("#modal-title") as HTMLHeadingElement;
+
+const modalClose = document.querySelector(".modal-close") as HTMLButtonElement;
+
+modalClose.addEventListener("click", () => {
+  modalContainer.classList.add("hidden");
+});
+
+modalToggler.forEach((btn) => {
+  btn.addEventListener("click", (e: Event) => {
+    const target = e.currentTarget as HTMLButtonElement;
+    modalTitle.textContent = target.textContent?.trim() ?? "";
+
+    modalContainer.classList.remove("hidden");
+  });
+});
